ddeda7f1a0e085d13769c1fcd195d673ff71592c
yglee730 <yglee730@naver.com>
2022-05-26 20:09:43 +0900

    change ui

--- main.py
+++ main.py
@@ -16,21 +16,41 @@ class MyApp(QMainWindow,formClass):
 
     def crawlStart(self):
         url = self.webSiteName.toPlainText()
+        response = requests.get(url, verify=False)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-        for page in range(1,30):
-            response = requests.get(url+str(page), verify=False)
-            soup = BeautifulSoup(response.content, 'html.parser')
+        response = requests.get(url, verify=False)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-            para1 = self.att1.toPlainText()
-            items = soup.find_all(para1)
+        tag1 = self.tag1.toPlainText()
+        att1 = self.att1.toPlainText()
+        value1 = self.value1.toPlainText()
 
-            self.crawlResult.appendPlainText("========="+str(page)+"==========")
+        st = "{},{{}:{}}".format(tag1, att1, value1)
+        items = soup.find_all(st)
+        print(items.text)
 
-            for item in items:
-                para2 = self.att2.toPlainText()
-                res = item.find(para2).text
-                self.crawlResult.appendPlainText(res)
-                print(res)
+        # for item in items:
+        #     para2 = self.att2.toPlainText()
+        #     res = item.find(para2).text
+        #     self.crawlResult.appendPlainText(res)
+        #     print(res)
+
+        # self.crawlResult.appendPlainText(items.text)
+        # for page in range(1,30):
+        #     response = requests.get(url+str(page), verify=False)
+        #     soup = BeautifulSoup(response.content, 'html.parser')
+        #
+        #     para1 = self.att1.toPlainText()
+        #     items = soup.find_all(para1)
+        #
+        #     self.crawlResult.appendPlainText("========="+str(page)+"==========")
+        #
+        #     for item in items:
+        #         para2 = self.att2.toPlainText()
+        #         res = item.find(para2).text
+        #         self.crawlResult.appendPlainText(res)
+        #         print(res)
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
